68b305dc865f895487b14d9ee787168c9c5394a1
szhakupbekov <szhakupbekov@woppay.com>
2018-01-08 16:34:14 +0600

    initial

---
+++ graphql/resolvers/mutation.js
@@ -0,0 +1,83 @@
+const { models } = require('../../models');
+
+module.exports = {
+  // User
+  createUser (root, { input }) {
+    return models.User.create(input);
+  },
+
+  updateUser (root, { id, input }) {
+    return models.User.findById(id)
+            .then(user => {
+              return user.update(input);
+            });
+  },
+
+  removeUser (root, { id }) {
+    return models.User.findById(id)
+            .then(user => user.destroy());
+  },
+
+  // Room
+  createRoom (root, { input }) {
+    return models.Room.create(input);
+  },
+
+  updateRoom (root, { id, input }) {
+    return models.Room.findById(id)
+            .then(room => {
+              return room.update(input);
+            });
+  },
+
+  removeRoom (root, { id }) {
+    return models.Room.findById(id)
+            .then(room => room.destroy());
+  },
+
+  // Event
+  createEvent (root, { input, usersIds, roomId }) {
+    return models.Event.create(input)
+            .then(event => {
+              event.setRoom(roomId);
+
+              return event.setUsers(usersIds)
+                    .then(() => event);
+            });
+  },
+
+  updateEvent (root, { id, input }) {
+    return models.Event.findById(id)
+            .then(event => {
+              return event.update(input);
+            });
+  },
+
+  removeUserFromEvent (root, { id, userId }) {
+    return models.Event.findById(id)
+            .then(event => {
+              event.removeUser(userId);
+              return event;
+            });
+  },
+
+  addUserToEvent (root, { id, userId }) {
+    return models.Event.findById(id)
+      .then(event => {
+        event.addUser(userId);
+        return event;
+      });
+  },
+
+  changeEventRoom (root, { id, roomId }) {
+    return models.Event.findById(id)
+            .then(event => {
+              return event.setRoom(roomId);
+            });
+  },
+
+  removeEvent (root, { id }) {
+    return models.Event.findById(id)
+            .then(event => event.destroy());
+  }
+};
---
+++ graphql/resolvers/query.js
@@ -0,0 +1,22 @@
+const { models } = require('../../models');
+
+module.exports = {
+  event (root, { id }) {
+    return models.Event.findById(id);
+  },
+  events () {
+    return models.Event.findAll();
+  },
+  user (root, { id }) {
+    return models.User.findById(id);
+  },
+  users () {
+    return models.User.findAll();
+  },
+  room (root, { id }) {
+    return models.Room.findById(id);
+  },
+  rooms () {
+    return models.Room.findAll();
+  }
+};
