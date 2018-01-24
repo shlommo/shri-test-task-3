eedd583d87d8da24221650c17dcfd26def791d59
szhakupbekov <szhakupbekov@woppay.com>
2018-01-24 13:06:55 +0600

    [eslint] api-service/index.js

--- client/js/api-service/index.js
+++ client/js/api-service/index.js
@@ -21,42 +21,41 @@ class ApiService {
   getAll() {
     const responseData = {};
     let getAllRequest = new Promise((resolve, reject) => {
-        resolve(this.getRooms());
-      }
-    );
+      resolve(this.getRooms());
+    });
     return getAllRequest
-      .then((res) => {
-        responseData.rooms = res.data.rooms;
-        return this.getEvents();
-      })
-      .then((res) => {
-        /**
-         * @typedef {Object} EventsSortedByDate
-         * @property {Event[]} timestamp Встречи отсортированные по дате.
-         */
-        let eventsSortedByDate = {};
-        for (let event of res.data.events) {
-          const dateStart = new Date(event.dateStart);
-          const dateStartDay = getDateValue(dateStart).day;
+        .then((res) => {
+          responseData.rooms = res.data.rooms;
+          return this.getEvents();
+        })
+        .then((res) => {
+          /**
+           * @typedef {Object} EventsSortedByDate
+           * @property {Event[]} timestamp Встречи отсортированные по дате.
+           */
+          let eventsSortedByDate = {};
+          for (let event of res.data.events) {
+            const dateStart = new Date(event.dateStart);
+            const dateStartDay = getDateValue(dateStart).day;
 
-          if (!eventsSortedByDate.hasOwnProperty(dateStartDay)) {
-            eventsSortedByDate[dateStartDay] = [event];
-          } else {
-            eventsSortedByDate[dateStartDay].push(event)
+            if (!eventsSortedByDate.hasOwnProperty(dateStartDay)) {
+              eventsSortedByDate[dateStartDay] = [event];
+            } else {
+              eventsSortedByDate[dateStartDay].push(event);
+            }
           }
-        }
-        responseData.events = eventsSortedByDate;
+          responseData.events = eventsSortedByDate;
 
-        // console.log(eventsSortedByDate);
-        return this.getUsers();
-      })
-      .then((res) => {
-        responseData.users = res.data.users;
-        return responseData;
-      })
-      .catch((error) => {
-        throw new Error(`Rejected: ${error}`)
-      });
+          // console.log(eventsSortedByDate);
+          return this.getUsers();
+        })
+        .then((res) => {
+          responseData.users = res.data.users;
+          return responseData;
+        })
+        .catch((error) => {
+          throw new Error(`Rejected: ${error}`);
+        });
   }
 
   createEvent(eventInput, usersIds, roomId) {
@@ -73,18 +72,16 @@ class ApiService {
 
   editEvent(eventId, eventInput, usersInput, roomId) {
     let editEvent = new Promise((resolve, reject) => {
-        resolve(this.removeEvent(eventId));
-      }
-    );
+      resolve(this.removeEvent(eventId));
+    });
     return editEvent
-      .then(() => {
-        return this.createEvent(eventInput, usersInput, roomId);
-      })
-      .catch((error) => {
-        throw new Error(`Rejected: ${error}`)
-      });
+        .then(() => {
+          return this.createEvent(eventInput, usersInput, roomId);
+        })
+        .catch((error) => {
+          throw new Error(`Rejected: ${error}`);
+        });
   }
-
 }
 
 export default new ApiService();
